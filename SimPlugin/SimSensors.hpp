d02c380d5866d5223cfb899f46b29faa4d1cd2b7
simondlevy <simon.d.levy@gmail.com>
2019-06-17 14:25:07 +0300

    Compute "body velocity" locally

--- SimPlugin/SimSensors.hpp
+++ SimPlugin/SimSensors.hpp
@@ -16,37 +16,51 @@
 
 class SimSensors : public hf::Sensor {
 
+    private:
+
+        // Helper
+        static void inertialToBody(float inertial[3], double rotation[3], float body[3])
+        {
+            double di[3] = {inertial[0], inertial[1], inertial[2]};
+            double bi[3] = {0};
+            MultirotorDynamics::inertialToBody(di, rotation, bi);
+            body[0] = bi[0];
+            body[1] = bi[1];
+            body[2] = bi[2];
+        }
+
     protected:
 
-    // We do all dynamcics => state conversion; subclasses just return sensor values
-    MultirotorDynamics * _dynamics;
+        // We do all dynamcics => state conversion; subclasses just return sensor values
+        MultirotorDynamics * _dynamics;
 
-    virtual bool ready(float time) override
-    {
-        (void) time;
-        return true;
-    }
+        virtual bool ready(float time) override
+        {
+            (void) time;
+            return true;
+        }
 
-    virtual void modifyState(hf::state_t & vehicleState, float time)
-    {
-        (void)time;
+        virtual void modifyState(hf::state_t & vehicleState, float time)
+        {
+            (void)time;
 
-        // Get vehicle state from dynamics
-        MultirotorDynamics::state_t dynamicsState = {0};
-        _dynamics->getState(dynamicsState);
+            // Get vehicle state from dynamics
+            MultirotorDynamics::state_t dynamicsState = {0};
+            _dynamics->getState(dynamicsState);
 
-        // Use vehicle state to modify Hackflight state values
-        vehicleState.location[2]    = -dynamicsState.pose.location[2]; // Negate for NED => ENU conversion
-        vehicleState.inertialVel[2] = -dynamicsState.inertialVel[2];
-        vehicleState.bodyVel[0] = dynamicsState.bodyVel[0];
-        vehicleState.bodyVel[1] = dynamicsState.bodyVel[1];
-    }
+            // Use vehicle state to modify Hackflight state values
+            vehicleState.location[2]    = -dynamicsState.pose.location[2]; // Negate for NED => ENU conversion
+            vehicleState.inertialVel[2] = -dynamicsState.inertialVel[2];
+
+            // Rotate inertial velocity into body frame for simulating optical flow
+            inertialToBody(vehicleState.inertialVel, dynamicsState.pose.rotation, vehicleState.bodyVel);
+        }
 
     public:
 
-    SimSensors(MultirotorDynamics * dynamics)
-    {
-        _dynamics = dynamics;
-    }
+        SimSensors(MultirotorDynamics * dynamics)
+        {
+            _dynamics = dynamics;
+        }
 
 }; // class SimSensor
