9b48ceaec94cab56ceb251b15cbc5402d4cb9b02
unknown <simondlevy>
2019-07-21 20:36:38 +0300

    Use index to name new windows (Issue #51)

--- VideoManager.cpp
+++ VideoManager.cpp
@@ -12,20 +12,25 @@
 
 class FExampleVideoManager : public FVideoManager {
 
+    private:
+
+        uint32_t _index;
+
     protected:
 
         virtual void processImage(cv::Mat image) override
         {
-            cv::imshow("MulticopterSim", image);
+            char windowName[100];
+            sprintf_s(windowName, "MulticopterSim%d", _index);
+            cv::imshow(windowName, image);
             cv::waitKey(1);
         }
 
     public:
 
-        FExampleVideoManager(UTextureRenderTarget2D * cameraRenderTarget) :
-            FVideoManager(cameraRenderTarget)
-
+        FExampleVideoManager(UTextureRenderTarget2D * cameraRenderTarget, uint32_t index) : FVideoManager(cameraRenderTarget)
         {
+            _index = index;
         }
 
         ~FExampleVideoManager()
@@ -37,9 +42,13 @@ static FExampleVideoManager * _videoManager;
 
 FLIGHTMODULE_API void videoManagersStart(UTextureRenderTarget2D * cameraRenderTarget1,UTextureRenderTarget2D * cameraRenderTarget2)
 {
-    _videoManager = new FExampleVideoManager(cameraRenderTarget1);
+    static uint32_t index;
+
+    _videoManager = new FExampleVideoManager(cameraRenderTarget1, index);
 
     (void)cameraRenderTarget2;
+
+    index++;
 }
 
 FLIGHTMODULE_API void videoManagersStop(void)
