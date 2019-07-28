82ed4ffff755118e924e8d059a15afa06404d898
unknown <simondlevy>
2019-07-28 19:15:15 +0300

    Uses Phantom helpher class

--- NullPhantomPawn.cpp
+++ NullPhantomPawn.cpp
@@ -8,27 +8,14 @@
 
 #include "NullPhantomPawn.h"
 
-// Structures to hold static mesh initializations
-DECLARE_STATIC_MESH(FFrameStatics, "Phantom/Frame.Frame", FrameStatics)
-DECLARE_STATIC_MESH(FProp1Statics, "Phantom/Prop.Prop", Prop1Statics)
-DECLARE_STATIC_MESH(FProp2Statics, "Phantom/Prop.Prop", Prop2Statics)
-DECLARE_STATIC_MESH(FProp3Statics, "Phantom/Prop.Prop", Prop3Statics)
-DECLARE_STATIC_MESH(FProp4Statics, "Phantom/Prop.Prop", Prop4Statics)
-
 ANullPhantomPawn::ANullPhantomPawn()
 {
-    _vehicle.build(this, FrameStatics.mesh.Get());
-
-    // Add propellers
-    addProp(1, +1, +1, Prop1Statics.mesh.Get());
-    addProp(2, -1, -1, Prop2Statics.mesh.Get());
-    addProp(3, +1, -1, Prop3Statics.mesh.Get());
-    addProp(4, -1, +1, Prop4Statics.mesh.Get());
+    _phantom.build(this);
 }
 
 void ANullPhantomPawn::PostInitializeComponents()
 {
-    _vehicle.PostInitializeComponents();
+    _phantom.PostInitializeComponents();
 
     Super::PostInitializeComponents();
 }
@@ -36,16 +23,14 @@ void ANullPhantomPawn::PostInitializeComponents()
 // Called when the game starts or when spawned
 void ANullPhantomPawn::BeginPlay()
 {
-    _flightManager = new FNullFlightManager(&_dynamics);
-
-    _vehicle.BeginPlay(_flightManager);
+    _phantom.BeginPlay(new FNullFlightManager(&_phantom.dynamics));
 
     Super::BeginPlay();
 }
 
 void ANullPhantomPawn::EndPlay(const EEndPlayReason::Type EndPlayReason)
 {
-    _flightManager = (FNullFlightManager *)FThreadedWorker::stopThreadedWorker(_flightManager);
+    _phantom.EndPlay();
 
     Super::EndPlay(EndPlayReason);
 }
@@ -53,16 +38,9 @@ void ANullPhantomPawn::EndPlay(const EEndPlayReason::Type EndPlayReason)
 // Called automatically on main thread
 void ANullPhantomPawn::Tick(float DeltaSeconds)
 {
-    _vehicle.Tick();
+    _phantom.Tick();
 
     Super::Tick(DeltaSeconds);
-}
-
-void ANullPhantomPawn::addProp(uint8_t index, int8_t x, int8_t y, UStaticMesh * mesh)
-{
-    float d = 0.12;
-
-    _vehicle.addProp(index-1, x*d, y*d, +.15, mesh);
 }
 
 
--- NullPhantomPawn.h
+++ NullPhantomPawn.h
@@ -8,10 +8,9 @@
 
 #pragma once
 
-#include "NullFlightManager.hpp"
+#include "../MainModule/vehicles/Phantom.h"
 
-#include "../MainModule/Vehicle.hpp"
-#include "../MainModule/dynamics/QuadXAP.hpp"
+#include "NullFlightManager.hpp"
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
@@ -25,32 +24,8 @@ class FLIGHTMODULE_API ANullPhantomPawn : public APawn {
 
         GENERATED_BODY()
 
-        MultirotorDynamics::params_t _params = {
-
-            // XXX These parameters are for a much larger vehicle
-            5.30216718361085E-05,   // b
-            2.23656692806239E-06,   // d
-            16.47,                  // m
-            0.6,                    // l
-            2,                      // Ix
-            2,                      // Iy
-            3,                      // Iz
-            3.08013E-04,            // Jr
-
-            // maxrpm, estimated
-            15000,                  
-
-            0                       // XXX motor acceleration }; 
-        }; 
-
-        QuadXAPDynamics _dynamics = QuadXAPDynamics(_params);
-
-        Vehicle _vehicle = Vehicle(&_dynamics);
-
-        // Threaded worker for flight control
-        FFlightManager * _flightManager = NULL;
-
-        void addProp(uint8_t index, int8_t x, int8_t y, UStaticMesh * mesh);
+        // Helper class
+        Phantom _phantom;
 
     protected:
 
